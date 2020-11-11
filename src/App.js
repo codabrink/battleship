f67cd6cbf492358cea5de6ba2ab169cb0515061f
Dakota <779390+codabrink@users.noreply.github.com>
2020-11-11 10:12:47 -0500

    run prettier, and a couple considerations

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
-import './App.css';
+import './App.css'
 
 import BattleHook from './battleHook'
 
 const player2Ships = {
-  "carrier":  [ "A10", "B10", "C10","D10","E10"],
-  "battleship":  [ "B3", "B4", "B5","B6"],
-  "destroyer":  [ "F3", "G3", "H3"],
-  "submarine":  [ "G1","H1","I1"],
-  "patrol":  ["A9", "B9"],
-};
+  carrier: ['A10', 'B10', 'C10', 'D10', 'E10'],
+  battleship: ['B3', 'B4', 'B5', 'B6'],
+  destroyer: ['F3', 'G3', 'H3'],
+  submarine: ['G1', 'H1', 'I1'],
+  patrol: ['A9', 'B9'],
+}
 const ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-const COLUMNS = [1,2,3,4,5,6,7,8,9,10]
+const COLUMNS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function App() {
   let battleship = BattleHook({ player2Ships })
 
-  let renderTile = (tile) => {
-    if (tile && tile.struck) {
-      if (tile.type) return <div className="tile-hit">X</div>
-      return <div className="tile-miss" />
-    }
-    return <div className="tile-empty" />
-  }
-
   return (
     <table>
       <tbody>
-        <tr><td />{COLUMNS.map(c => <td key={c}>{c}</td>)}</tr>
-        {ROWS.map(r => {
-          return (<tr key={r}>
-            <td>{r}</td>
-            {COLUMNS.map(c => {
-              let tile = battleship.tile(r, c)
-              return (<td key={r + c} onClick={() => battleship.fire(r, c)} className="tile">
-                {renderTile(tile)}
-              </td>)
-            })}
-          </tr>)
+        <tr>
+          <td />
+          {COLUMNS.map((c) => (
+            <td key={c}>{c}</td>
+          ))}
+        </tr>
+        {ROWS.map((r) => {
+          return (
+            <tr key={r}>
+              <td>{r}</td>
+              {COLUMNS.map((c) => {
+                let tile = battleship.tile(r, c)
+                return (
+                  <td
+                    key={r + c}
+                    onClick={() => battleship.fire(r, c)}
+                    className="tile"
+                  >
+                    <Tile tile={tile} />
+                  </td>
+                )
+              })}
+            </tr>
+          )
         })}
       </tbody>
     </table>
-  );
+  )
 }
 
-export default App;
+function Tile({ tile }) {
+  if (tile && tile.struck) {
+    if (tile.type) return <div className="tile-hit">X</div>
+    return <div className="tile-miss" />
+  }
+  return <div className="tile-empty" />
+}
+
+export default App
--- src/battleHook.js
+++ src/battleHook.js
@@ -1,13 +1,13 @@
-import {useReducer} from 'react'
+import { useReducer } from 'react'
 
 export default function BattleHook({ player2Ships }) {
-  let initState = {tiles: {}}
+  let initState = { tiles: {} }
 
   for (let [type, coordinates] of Object.entries(player2Ships)) {
     for (let coordinate of coordinates) {
       initState.tiles[coordinate] = {
         type,
-        struck: false
+        struck: false,
       }
     }
   }
@@ -15,26 +15,28 @@ export default function BattleHook({ player2Ships }) {
   let [data, dispatch] = useReducer(reducer, initState)
 
   return {
-    tile: (row, column) => data.tiles[`${row}${column}`],
-    fire: (row, column) => {dispatch({type: 'fire', row, column})},
-    ...data
+    tile: (row, column) => data.tiles[row + column],
+    fire: (row, column) => {
+      dispatch({ type: 'fire', row, column })
+    },
+    ...data,
   }
 }
 function reducer(state, action) {
   switch (action.type) {
     case 'fire':
       let coordinates = action.row + action.column
-      console.log("Firing on ", coordinates)
+      console.log('Firing on ', coordinates)
 
       return {
         ...state,
         tiles: {
           ...state.tiles,
           [coordinates]: {
-            ...state.tiles[coordinates] || { type: null },
-            struck: true
-          }
-        }
+            ...(state.tiles[coordinates] || { type: null }),
+            struck: true,
+          },
+        },
       }
     default:
       return state
