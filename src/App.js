a5789d6d62c56a55b93f8f7492331ab2d9b3c1b4
Dakota <779390+codabrink@users.noreply.github.com>
2020-11-18 17:50:55 -0500

    Required updates

--- src/App.js
+++ src/App.js
@@ -1,59 +1,81 @@
-import './App.css'
+import "./App.css";
+import BattleHook from "./battleHook";
+import React, { useEffect } from "react";
 
-import BattleHook from './battleHook'
+const ROWS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const COLUMNS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const player2Ships = {
-  carrier: ['A10', 'B10', 'C10', 'D10', 'E10'],
-  battleship: ['B3', 'B4', 'B5', 'B6'],
-  destroyer: ['F3', 'G3', 'H3'],
-  submarine: ['G1', 'H1', 'I1'],
-  patrol: ['A9', 'B9'],
-}
-const ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-const COLUMNS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const PLAYERS = 2;
 
 function App() {
-  let battleship = BattleHook({ player2Ships })
+  let battleship = BattleHook({});
 
+  useEffect(() => {
+    let { lastPlayer, lastFireStruck } = battleship;
+    if (
+      (lastPlayer === "2" && lastFireStruck) ||
+      (lastPlayer === "1" && !lastFireStruck)
+    ) {
+      battleship.computerFire();
+    }
+  }, [battleship.move]);
+
+  let boards = [];
+  for (let i = 1; i <= PLAYERS; i++) {
+    boards.push(<Board battleship={battleship} player={i} />);
+  }
+
+  return <div style={{ display: "flex" }}>{boards}</div>;
+}
+
+function Board({ battleship, player }) {
+  if (!battleship.gameId) return <span>Loading...</span>;
+  if (battleship.won) return <span>Player {battleship.won} won!</span>;
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td />
-          {COLUMNS.map((c) => (
-            <td key={c}>{c}</td>
-          ))}
-        </tr>
-        {ROWS.map((r) => {
-          return (
-            <tr key={r}>
-              <td>{r}</td>
-              {COLUMNS.map((c) => {
-                let tile = battleship.tile(r, c)
-                return (
-                  <td
-                    key={r + c}
-                    onClick={() => battleship.fire(r, c)}
-                    className="tile"
-                  >
-                    <Tile tile={tile} />
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
+    <div>
+      <h4 style={{ textAlign: "center" }}>Player {player}</h4>
+      <table>
+        <tbody>
+          <tr>
+            <td />
+            {COLUMNS.map((c) => (
+              <td key={c}>{c}</td>
+            ))}
+          </tr>
+          {ROWS.map((r) => {
+            return (
+              <tr key={r}>
+                <td>{r}</td>
+                {COLUMNS.map((c) => {
+                  let tile = battleship.tile(r, c, player);
+                  return (
+                    <td
+                      key={r + c}
+                      onClick={() => {
+                        // add if not already clicked
+                        battleship.fire(r, c, player);
+                      }}
+                      className="tile"
+                    >
+                      <Tile tile={tile} />
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 function Tile({ tile }) {
   if (tile && tile.struck) {
-    if (tile.type) return <div className="tile-hit">X</div>
-    return <div className="tile-miss" />
+    if (tile.hit) return <div className="tile-hit">X</div>;
+    return <div className="tile-miss" />;
   }
-  return <div className="tile-empty" />
+  return <div className="tile-empty" />;
 }
 
-export default App
+export default App;
--- src/battleHook.js
+++ src/battleHook.js
@@ -1,44 +1,125 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from "react";
+import { player1Ships, player2Moves } from "./const";
+const API = "https://solv-battleship.herokuapp.com";
 
-export default function BattleHook({ player2Ships }) {
-  let initState = { tiles: {} }
+export default function BattleHook({ players = 2 }) {
+  let initState = {
+    won: 0,
+    gameId: "",
+    move: 0,
+    lastFireStruck: false,
+    lastPlayer: "0",
+    tiles: {},
+    players,
+    player2MoveIndex: 0,
+  };
 
-  for (let [type, coordinates] of Object.entries(player2Ships)) {
+  initState.tiles["1"] = {};
+  for (let [type, coordinates] of Object.entries(player1Ships)) {
     for (let coordinate of coordinates) {
-      initState.tiles[coordinate] = {
+      initState.tiles["1"][coordinate] = {
         type,
         struck: false,
-      }
+      };
     }
   }
+  initState.tiles["2"] = {};
+  let [data, dispatch] = useReducer(reducer, initState);
 
-  let [data, dispatch] = useReducer(reducer, initState)
+  useEffect(() => {
+    fetch(`${API}/api/v1/battleship/createGame`, { method: "POST" })
+      .then((r) => r.json())
+      .then((r) => {
+        dispatch({ type: "setGameId", gameId: r.gameId });
+      });
+  }, []);
 
   return {
-    tile: (row, column) => data.tiles[row + column],
-    fire: (row, column) => {
-      dispatch({ type: 'fire', row, column })
+    tile: (row, column, player) => {
+      return data.tiles[player + ""][row + column];
+    },
+    fire: (row, column, player) => {
+      post("/api/v1/battleship/fire", {
+        gameId: data.gameId,
+        coordinate: row + column,
+      }).then((r) => {
+        dispatch({
+          type: "fire",
+          row,
+          column,
+          player: player + "",
+          status: r.status,
+        });
+      });
+    },
+    computerFire: () => {
+      post("/api/v1/battleship/requestMove", {
+        gameId: data.gameId,
+      }).then((r) => {
+        dispatch({ type: "fire", coordinates: r.coordinate, player: "2" });
+        dispatch({ type: "incrementPlayer2MoveIndex" });
+      });
     },
     ...data,
-  }
+  };
 }
+
+function post(url, body) {
+  return fetch(`${API}${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then((r) => r.json());
+}
+
 function reducer(state, action) {
   switch (action.type) {
-    case 'fire':
-      let coordinates = action.row + action.column
-      console.log('Firing on ', coordinates)
-
+    case "setGameId":
       return {
         ...state,
+        gameId: action.gameId,
+      };
+    case "fire":
+      let coordinates = action.coordinates || action.row + action.column;
+      console.log("Firing on ", coordinates);
+
+      let hit =
+        action.status === "hit" ||
+        (action.player === "2" && state.tiles["1"][coordinates]?.type);
+
+      let _state = {
+        ...state,
+        move: state.move + 1,
+        lastPlayer: action.player,
+        lastFireStruck: hit,
         tiles: {
           ...state.tiles,
-          [coordinates]: {
-            ...(state.tiles[coordinates] || { type: null }),
-            struck: true,
+          [action.player]: {
+            ...state.tiles[action.player],
+            [coordinates]: {
+              ...(state.tiles[action.player][coordinates] || { type: null }),
+              struck: true,
+              hit,
+            },
           },
         },
+      };
+
+      for (let [_, t] of Object.entries(_state.tiles)) {
+        let tiles = Object.values(t);
+        let hits = tiles.reduce((acc, t) => (t.hit ? acc + 1 : acc), 0);
+        if (hits === 17) state = { ...state, won: action.player };
       }
+
+      return _state;
+    case "incrementPlayer2MoveIndex":
+      return {
+        ...state,
+        player2MoveIndex: state.player2MoveIndex + 1,
+      };
     default:
-      return state
+      return state;
   }
 }
